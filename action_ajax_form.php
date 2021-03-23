693f0539a52a2f51b3f2421e17396b26153c35a2
gohsifus <gohsifus@gmail.com>
2021-03-23 15:21:55 +0300

    refactor/Немного подправлен код

--- action_ajax_form.php
+++ action_ajax_form.php
@@ -1,40 +1,51 @@
 <?php
-function writeToLogFile($str){
+/**
+ * Логирование
+ * @param $messageLog
+ */
+function writeToLogFile($messageLog)
+{
     $file = fopen("logFile.txt", 'a');
-    fwrite($file, "\n".date("Y-m-d h:i:s :").$str);
+    fwrite($file, "\n" . date("Y-m-d h:i:s :") . $messageLog);
     fclose($file);
 }
 
-function checkUser($data, $username, $email){
-    foreach ($data['users'] as $user){
-        if($user['name'] == $username && $user['email'] == $email){
+/**
+ * Проверка авторизации
+ * @param $data источник данных о пользователях
+ * @param $username
+ * @param $email
+ * @return bool
+ */
+function checkUser($data, $username, $email)
+{
+    foreach ($data['users'] as $user) {
+        if ($user['name'] == $username && $user['email'] == $email) {
             return true;
         }
     }
     return false;
 }
 
-/*
- * Массив с пользователями
- * **/
+//Массив с пользователями
 $data = array(
     'users' => [
-        '1' =>[
+        '1' => [
             'id' => 1,
             'name' => 'Nikita Volkov',
             'email' => 'nikita@gmail.com'
         ],
-        '2' =>[
+        '2' => [
             'id' => 2,
             'name' => 'Kirill Cherkov',
             'email' => 'kirill@gmail.com'
         ],
-        '3' =>[
+        '3' => [
             'id' => 1,
             'name' => 'Danya Davadov',
             'email' => 'danya@gmail.com'
         ],
-        '4' =>[
+        '4' => [
             'id' => 1,
             'name' => 'Dasha Klimushina',
             'email' => 'dahsa@gmail.com'
@@ -42,20 +53,18 @@ $data = array(
     ],
 );
 
-/*
- * Обработка данных из формы
- * */
+// Обработка данных из формы
 if (isset($_POST['password']) && isset($_POST['passwordConfirm']) && isset($_POST['email'])) {
-    $user = $_POST['username'].' '.$_POST['surname'];
+    $user = $_POST['username'] . ' ' . $_POST['surname'];
 
     $result = array(); //Ответ сервера
-    if(strpos($_POST['email'],'@')) {   //Проверка email
+    if (strpos($_POST['email'], '@')) {   //Проверка email
 
         if ($_POST['passwordConfirm'] != $_POST['password']) { //Проверка подтверждения пароля
             $result['error'] = 'Password not confirmed';
             writeToLogFile("Неудачная регистрация пользователся: $user \n   ' Пароль не подтвержден '");
         } else {
-            if(checkUser($data, $_POST['username'].' '.$_POST['surname'], $_POST['email'])){  //Проверка на сущесвование в массиве
+            if (checkUser($data, $_POST['username'] . ' ' . $_POST['surname'], $_POST['email'])) {  //Проверка на сущесвование в массиве
                 $result['error'] = 'false';
                 writeToLogFile("Успешная регистрация пользователя: $user \n   ' ==================== '");
             } else {
@@ -65,11 +74,10 @@ if (isset($_POST['password']) && isset($_POST['passwordConfirm']) && isset($_POS
         }
 
     } else {
-       $result['error'] = 'Email must contain \'@\'';
+        $result['error'] = 'Email must contain \'@\'';
         writeToLogFile("Неудачная регистрация пользователся: $user \n   ' Email не содержит '@' '");
     }
 
     // Переводим массив в JSON
     echo json_encode($result);
 }
-?>
